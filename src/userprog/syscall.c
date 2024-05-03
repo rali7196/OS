0de4a12ba61616bf6b754f28fd29dc7d0d56eef2
MichaelXzj <45219543+MichaelXiongZJ@users.noreply.github.com>
2024-05-03 16:41:17 -0700

    added user memory checking and file system lock in syscall

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,14 +10,17 @@
 #include "filesys/file.h"
 #include "devices/input.h"
 #include "devices/shutdown.h"
+#include "threads/synch.h"
 
 static void syscall_handler (struct intr_frame *);
 static bool validate_user_pointer (const void *ptr);  // implemented for project 2
+static struct lock fs_lock;  // lock for file system operations
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init(&fs_lock);
 }
 
 static void
@@ -44,7 +47,7 @@ syscall_handler (struct intr_frame *f UNUSED)
     if (!validate_user_pointer(file_name)){
       printf("Invalid file name in SYS_EXEC\n"); // debug
       f->eax = -1;
-      thread_exit();
+      return; // thread_exit();
     }
     f->eax = process_execute(file_name); // will return -1 if error
   }
@@ -55,14 +58,19 @@ syscall_handler (struct intr_frame *f UNUSED)
   else if (syscall_num == SYS_CREATE){  // create a new file with the given name and size
     char* file_name = *((char**)f->esp + 1);
     unsigned initial_size = *((unsigned*)f->esp + 2);
+    lock_acquire(&fs_lock);
     f->eax = filesys_create(file_name, initial_size);
+    lock_release(&fs_lock);
   }
   else if (syscall_num == SYS_REMOVE){  // remove the file with the given name
     char* file_name = *((char**)f->esp + 1);
+    lock_acquire(&fs_lock);
     f->eax = filesys_remove(file_name);
+    lock_release(&fs_lock);
   }
   else if (syscall_num == SYS_OPEN){
     char* file_name = *((char**)f->esp + 1);
+    lock_acquire(&fs_lock);
     struct file* file = filesys_open(file_name);
     if (!file){
       f->eax = -1;
@@ -75,47 +83,74 @@ syscall_handler (struct intr_frame *f UNUSED)
         }
       }
     }
+    lock_release(&fs_lock);
   }
   else if (syscall_num == SYS_FILESIZE){  // return the size of the file with the given fd
     int fd = *((int*)f->esp + 1);
+    lock_acquire(&fs_lock);
     f->eax = file_length(thread_current()->file_descriptors_table[fd]);
+    lock_release(&fs_lock);
   }
   else if (syscall_num == SYS_READ){  
     int fd = *((int*)f->esp + 1);
     void* buffer = *((void**)f->esp + 2);
     unsigned size = *((unsigned*)f->esp + 3);
+    if (!validate_user_pointer(buffer) || !validate_user_pointer(buffer + size - 1)){ // check buffer validity
+      printf("Invalid buffer in SYS_READ\n"); // debug
+      f->eax = -1;
+      return; //thread_exit();
+    }
     if (fd == 0){ // read from input_getc
       for (unsigned i = 0; i < size; i++){
         *((char*)buffer + i) = input_getc();
       }
       f->eax = size;
     } else {  // read from the file with the given fd
+      lock_acquire(&fs_lock);
       f->eax = file_read(thread_current()->file_descriptors_table[fd], buffer, size);
+      lock_release(&fs_lock);
     }
   }
   else if (syscall_num == SYS_WRITE){
     int fd = *((int*)f->esp + 1);
     void* buffer = *((void**)f->esp + 2);
     unsigned size = *((unsigned*)f->esp + 3);
+    if (!validate_user_pointer(buffer) || !validate_user_pointer(buffer + size - 1)){ // check buffer validity
+      printf("Invalid buffer in SYS_WRITE\n"); // debug
+      f->eax = -1;
+      thread_exit();
+    }
     if (fd == 1){  // write to the console
       putbuf(buffer, size);
       f->eax = size;
     } else {  // write to the file with the given fd
+      lock_acquire(&fs_lock);
       f->eax = file_write(thread_current()->file_descriptors_table[fd], buffer, size);
+      lock_release(&fs_lock);
     }
   }
   else if (syscall_num == SYS_SEEK){
     int fd = *((int*)f->esp + 1);
+    if (!validate_user_pointer((void*)(f->esp + 2))) {
+      printf("Invalid position in SYS_SEEK\n"); // debug
+      thread_exit();
+    }
     unsigned position = *((unsigned*)f->esp + 2);
+    lock_acquire(&fs_lock);
     file_seek(thread_current()->file_descriptors_table[fd], position);
+    lock_release(&fs_lock);
   }
   else if (syscall_num == SYS_TELL){
     int fd = *((int*)f->esp + 1);
+    lock_acquire(&fs_lock);
     f->eax = file_tell(thread_current()->file_descriptors_table[fd]);
+    lock_release(&fs_lock);
   }
   else if (syscall_num == SYS_CLOSE){
     int fd = *((int*)f->esp + 1);
+    lock_acquire(&fs_lock);
     file_close(thread_current()->file_descriptors_table[fd]);
+    lock_release(&fs_lock);
     thread_current()->file_descriptors_table[fd] = NULL;
   }
   else {  // to-do: add proper error handling
